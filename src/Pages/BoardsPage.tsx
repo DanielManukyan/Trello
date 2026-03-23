83a5480925589c8375458457464074ee720a0991
Daniel <danielmanukyan1000@gmail.com>
2026-03-23 15:30:09 +0400

    create header and boards page

---
+++ src/Pages/BoardsPage.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Headers/Header";
+
+function BoardsPage() {
+    return ( 
+        <div>
+            <Header />
+        </div>
+     );
+}
+
+export default BoardsPage;
