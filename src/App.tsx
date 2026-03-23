93e2703718d99aa6d93232f39f39df5b1f4e7258
Daniel <danielmanukyan1000@gmail.com>
2026-03-23 15:29:39 +0400

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import "/index.css"
+import "./index.css"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import BoardsPage from "./Pages/BoardsPage";
 
 function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<BoardsPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
